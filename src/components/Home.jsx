e9f1a8b45d916a4db7094dc8572a3cc8f921e1dc
Tanisha <officialtanisha20@gmail.com>
2026-02-28 12:57:33 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { THEME, GLOBAL_STYLE } from "../theme";
+import { THEME, GLOBAL_STYLE } from "./theme";
 
 const FEATURES = [
   {
